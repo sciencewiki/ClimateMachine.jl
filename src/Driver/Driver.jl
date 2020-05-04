af1a47f876da283165f4defe197524a7ff688631
Jeremy E Kozdon <jekozdon@nps.edu>
2020-05-04 15:43:58 +0000

    fixup! Use remainder_DGModel with underlying DGModels

--- src/Driver/Driver.jl
+++ src/Driver/Driver.jl
@@ -18,7 +18,6 @@ using ..DGmethods:
     vars_state_conservative,
     vars_state_auxiliary,
     update_auxiliary_state!,
-    RemainderModel,
     remainder_DGModel
 using ..DGmethods.NumericalFluxes
 using ..HydrostaticBoussinesq
