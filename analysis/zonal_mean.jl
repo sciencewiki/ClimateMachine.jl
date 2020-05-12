ee50684a261866db1dad111565d62cc73c74f147
lenka <lenka@caltech.edu>
2020-05-12 08:29:50 -0700

    modif zonal_mean.jl

--- analysis/zonal_mean.jl
+++ analysis/zonal_mean.jl
@@ -6,8 +6,9 @@ using Printf
 
 
 # analysis folder
-ana_folder = "/central/scratch/bischtob/heldsuarez/nc"
-#ana_folder = "/Users/lenka/Desktop/CLIMA/output"
+#ana_folder = "/central/scratch/bischtob/heldsuarez/nc"
+ana_folder = "/Users/lenka/Desktop/CLIMA/output"
+ana_folder = "/groups/esm/lenka/CLIMA/output"
 num_avg_a = 350
 num_avg_b = num_avg_a + 12
 num_avg = num_avg_b - num_avg_a + 1
@@ -27,29 +28,29 @@ function read_ncfiles(filename, filename_aux, pinfo)
   rad = dfile["rad"].var[:] .- 6371000.0
   path = joinpath(ana_folder, filename_aux)
   dfile = Dataset(path)
-  T = dfile["moisture.air_T"].var[:] 
-  return lon, lat, rad, ρ, u, e, T
+  #T = dfile["moisture.air_T"].var[:] 
+  return lon, lat, rad, ρ, u, e
 end
 
 
 
 
 # get variables
-lon, lat, rad, ρ, u, e, T = read_ncfiles("hs_step0001.nc", "hs_step0001_aux.nc", true)
+lon, lat, rad, ρ, u, e = read_ncfiles("hs_step0001.nc", "hs_step0001_aux.nc", true)
 
 # load data into arrays for averaging
 ρ_a = zeros(num_avg, size(rad)[1], size(lat)[1], size(lon)[1])
 u_a = zeros(num_avg, size(rad)[1], size(lat)[1], size(lon)[1])
 e_a = zeros(num_avg, size(rad)[1], size(lat)[1], size(lon)[1])
 T_a = zeros(num_avg, size(rad)[1], size(lat)[1], size(lon)[1])
-for i in 1:num_avg
+for i in 1:9
   ind = num_avg_a + i - 1
-  lon, lat, rad, ρ_a[i,:,:,:], u_a[i,:,:,:], e_a[i,:,:,:], T_a[i,:,:,:] = read_ncfiles(@sprintf("hs_step000%s.nc",i), @sprintf("hs_step000%s_aux.nc",i), false)
+  lon, lat, rad, ρ_a[i,:,:,:], u_a[i,:,:,:], e_a[i,:,:,:] = read_ncfiles(@sprintf("hs_step000%s.nc",i), @sprintf("hs_step000%s_aux.nc",i), false)
 end
 
 # average data
 u_zm = dropdims( mean(u_a, dims = (1,4) ) , dims = (1,4) )
-T_zm = dropdims( mean(T_a, dims = (1,4) ) , dims = (1,4) )
+T_zm = dropdims( mean(e_a, dims = (1,4) ) , dims = (1,4) )
 
 # plot
 contourf(lat*180/3.14 .- 90.0, rad, u_zm, levels=10)
