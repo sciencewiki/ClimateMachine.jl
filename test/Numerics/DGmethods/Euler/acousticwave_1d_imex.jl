08b8081526f7649ee49afc36b1698c0e526f1ff7
Lucas C Wilcox <lucas@swirlee.com>
2020-05-13 13:36:55 -0700

    Just run Float64

--- test/Numerics/DGmethods/Euler/acousticwave_1d_imex.jl
+++ test/Numerics/DGmethods/Euler/acousticwave_1d_imex.jl
@@ -71,7 +71,7 @@ function main()
     expected_result[Float32] = 9.5064378310656000e+13
     expected_result[Float64] = 9.5073452847081828e+13
 
-    for FT in (Float32, Float64)
+    for FT in (Float64,)
         result = run(
             mpicomm,
             polynomialorder,
